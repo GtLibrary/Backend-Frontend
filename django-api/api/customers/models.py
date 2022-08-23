f440f0dd0e18d78417a945bfd01006000cc01b16
alzmob <yunayamda3@gmail.com>
2022-08-24 03:06:28 +0900

    backend database file updated

--- django-api/api/customers/models.py
+++ django-api/api/customers/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 class Customers(models.Model):
     wallet_address = models.CharField(max_length=200, default='')
-    book_id = models.BigIntegerField(default=0)
     pub_date = models.DateTimeField('date published',auto_now=True, null=False, blank=False)
     class Meta:
         ordering = ['id']
