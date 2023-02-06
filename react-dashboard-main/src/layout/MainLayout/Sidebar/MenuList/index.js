276a2fc19f4eaf65fe661096f8d6d50358cdc411
Topdev529 <akirahumadaking@gmail.com>
2023-02-06 21:00:37 +0900

    merge

--- react-dashboard-main/src/layout/MainLayout/Sidebar/MenuList/index.js
+++ react-dashboard-main/src/layout/MainLayout/Sidebar/MenuList/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 // material-ui
 import { Typography } from '@material-ui/core';
@@ -6,10 +7,24 @@ import { Typography } from '@material-ui/core';
 // project imports
 import NavGroup from './NavGroup';
 import menuItem from './../../../../menu-items';
+import nopermission from '../../../../menu-items/nopermission';
 
 //-----------------------|| SIDEBAR MENU LIST ||-----------------------//
 
 const MenuList = () => {
+    const userinfo = useSelector((state) => state.account);
+    const navPermission = nopermission.items.map((item) => {
+        switch (item.type) {
+            case 'group':
+                return <NavGroup key={item.id} item={item} />;
+            default:
+                return (
+                    <Typography key={item.id} variant="h6" color="error" align="center">
+                        Menu Items Error
+                    </Typography>
+                );
+        }
+    })
     const navItems = menuItem.items.map((item) => {
         switch (item.type) {
             case 'group':
@@ -23,7 +38,12 @@ const MenuList = () => {
         }
     });
 
-    return navItems;
+    if(userinfo.is_staff) {
+        return navItems;
+    } else {
+        return navPermission;
+    }
+
 };
 
 export default MenuList;
