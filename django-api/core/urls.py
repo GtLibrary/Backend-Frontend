41cb8cafffeb1d912c849074a7be99cfabcaf2dc
Topdev529 <akirahumadaking@gmail.com>
2023-02-06 14:08:23 +0900

    remove unused old backend

--- django-api/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path("api/", include(("api.routers", "api"), namespace="api")),
-]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
