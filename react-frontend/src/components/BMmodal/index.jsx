5da6bd66afa2302a899869fcfbb5835daa265511
alzmob <yunayamda3@gmail.com>
2023-03-04 04:35:40 +0900

    update for bookmark function on frontend

--- react-frontend/src/components/BMmodal/index.jsx
+++ react-frontend/src/components/BMmodal/index.jsx
@@ -3,6 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { useWeb3React } from "@web3-react/core";
 import Web3 from 'web3';
+import axios from "axios";
 import printingpress_abi from "../../utils/contract/PrintingPress.json"
 import NBT_abi from "../../utils/contract/BookTradable.json"
 import Marketplace_abi from "../../utils/contract/MarketPlace.json"
@@ -13,7 +14,7 @@ function BMdetailModal(props) {
     const { account } = useWeb3React();
   
     const web3 = new Web3(window.ethereum);
-    const { product, curserial_num } = props
+    const { id, curserial_num } = props
     const printpress_abi = printingpress_abi;
     const marketplace_abi = Marketplace_abi;
     const cc_abi = CC_abi;
@@ -25,24 +26,49 @@ function BMdetailModal(props) {
     const cc_initial_balance = process.env.REACT_APP_CC_INITIAL_BALANCE;
     const ccTotalSupplyStart = process.env.REACT_APP_CCTOTALSUPPLYSTART;
 
-    const { datamine, bookmark_price, bm_contract_address, hb_contract_address, hardbound_price } = product
     const tokenid = curserial_num;
     
     const [dexrate, setDexrate] = useState(0);
     const [stakerate, setStakerate] = useState(0);
     const [tosendaddress, setTosendaddress] = useState('');
     const [sellerprice, setSellerprice] = useState(0);
+    const [bookmarks, setBookmarks] = useState([]);
+    const [hbcontractaddress, setHbcontractaddress] = useState('');
+    const [datamine, setDatamine] = useState('')
+    const [hbprice, setHbprice] = useState(0)
     const [xmsprate, setXmsprate] = useState(0);
     const [ccrate, setCcrate] = useState(0);
     const [bmContractowner, setBmContractowner] = useState('');
-    const NBTcontract = new web3.eth.Contract(NBT_abi,bm_contract_address);
 
     useEffect(() => {
-        async function setcontractowner() {
-            const contractOwner = await NBTcontract.methods.owner().call();
-            setBmContractowner(contractOwner)
+        const bookurl = process.env.REACT_APP_API + `bookdata/${id}`;
+        const getBook = async () => {        
+          const config = {
+            method: "get",
+            url: bookurl,
+          };
+          await axios(config)
+            .then((res) => {
+                const bookinfo = res.data[0];
+                setBookmarks(bookinfo.bm_listdata);
+                setDatamine(bookinfo.datamine);
+                setHbcontractaddress(bookinfo.hb_contract_address)
+                setHbprice(bookinfo.hardbound_price);
+            })
+            .catch((error) => console.log(error));
         }
-        setcontractowner()
+        
+        getBook();
+      }, []);
+
+    useEffect(() => {
+        // const bm_contract_address = "";
+        // const NBTcontract = new web3.eth.Contract(NBT_abi,bm_contract_address);
+        // async function setcontractowner() {
+        //     const contractOwner = await NBTcontract.methods.owner().call();
+        //     setBmContractowner(contractOwner)
+        // }
+        // setcontractowner()
     }, [])
 
     let user_wallet;
@@ -52,12 +78,12 @@ function BMdetailModal(props) {
         user_wallet = ""
     }
 
-    const buyBookMark = async () => {
+    const buyBookMark = async (bmcontract_address, bm_price) => {
         const printpress_contract = new web3.eth.Contract(printpress_abi, printpress_address);
 
-        await printpress_contract.methods.buyBook(bm_contract_address).send({
+        await printpress_contract.methods.buyBook(bmcontract_address).send({
             from: user_wallet,
-			value: web3.utils.toWei(String(bookmark_price)),
+			value: web3.utils.toWei(String(bm_price)),
             gas: 800000
         });
     }
@@ -65,34 +91,34 @@ function BMdetailModal(props) {
     const buyBookHardbound = async () => {
         const printpress_contract = new web3.eth.Contract(printpress_abi, printpress_address);
 
-        await printpress_contract.methods.buyBook(hb_contract_address).send({
+        await printpress_contract.methods.buyBook(hbcontractaddress).send({
             from: user_wallet,
-			value: web3.utils.toWei(String(hardbound_price)),
+			value: web3.utils.toWei(String(hbprice)),
             gas: 800000
         });
     }
 
     const sellthisbookmark = async () => {
 
-        const NBTcontract = new web3.eth.Contract(NBT_abi,bm_contract_address);
-        // const contractOwner = await NBTcontract.methods.owner().call();
+        // const NBTcontract = new web3.eth.Contract(NBT_abi,bm_contract_address);
+        // // const contractOwner = await NBTcontract.methods.owner().call();
         
-        const approved = await NBTcontract.methods.getApproved(tokenid).call();
+        // const approved = await NBTcontract.methods.getApproved(tokenid).call();
     
-        if (approved !== marketPlaceAddress){
-            const tokenOnwner = await NBTcontract.methods.ownerOf(tokenid).call();
-            console.log("tokenOnwner:", tokenOnwner);
-            await approveMarketPlace(bm_contract_address, tokenid);
-            for(var i = 0; i < 10; i++) {
-                const approved = await NBTcontract.methods.getApproved(tokenid).call();
-                if (approved.toLowerCase() === marketPlaceAddress.toLowerCase()) {
-                    break;
-                }
-                await sleep(3000);
-            }
-        }
+        // if (approved !== marketPlaceAddress){
+        //     const tokenOnwner = await NBTcontract.methods.ownerOf(tokenid).call();
+        //     console.log("tokenOnwner:", tokenOnwner);
+        //     await approveMarketPlace(bm_contract_address, tokenid);
+        //     for(var i = 0; i < 10; i++) {
+        //         const approved = await NBTcontract.methods.getApproved(tokenid).call();
+        //         if (approved.toLowerCase() === marketPlaceAddress.toLowerCase()) {
+        //             break;
+        //         }
+        //         await sleep(3000);
+        //     }
+        // }
     
-        const offering = await placeOfferingOwner(user_wallet, bm_contract_address, tokenid, sellerprice);
+        // const offering = await placeOfferingOwner(user_wallet, bm_contract_address, tokenid, sellerprice);
     }
 
     const approveMarketPlace = async (contractaddress, tokenid) => {
@@ -108,15 +134,15 @@ function BMdetailModal(props) {
 
     const sendthisbookmark = async () => {
         
-        const NBTcontract = new web3.eth.Contract(NBT_abi, bm_contract_address);
-        var  tokenOwner = await NBTcontract.methods.ownerOf(tokenid).call();
-        console.log("tokenOwner:", tokenOwner);
+        // const NBTcontract = new web3.eth.Contract(NBT_abi, bm_contract_address);
+        // var  tokenOwner = await NBTcontract.methods.ownerOf(tokenid).call();
+        // console.log("tokenOwner:", tokenOwner);
     
-        if(user_wallet.toLowerCase() !== tokenOwner.toLowerCase()) {
-            // alert("You are not the owner of this token. You cannot give it away... Trying anyway. Failure likely...");
-        }
+        // if(user_wallet.toLowerCase() !== tokenOwner.toLowerCase()) {
+        //     // alert("You are not the owner of this token. You cannot give it away... Trying anyway. Failure likely...");
+        // }
     
-        await NBTcontract.methods.transferFrom(user_wallet, tosendaddress, tokenid).send({from: user_wallet});
+        // await NBTcontract.methods.transferFrom(user_wallet, tosendaddress, tokenid).send({from: user_wallet});
     }
 
     const sleep = (ms) => {
@@ -196,14 +222,24 @@ function BMdetailModal(props) {
         </Modal.Header>
         <Modal.Body>
             <div>
-                <p>Price for bookmark: {curserial_num} {datamine}</p>
-                <p>Contract Address: {bm_contract_address}</p>
-                <p>Bookmark Price : {bookmark_price}</p>
-                <button type="button" className="btn btn-primary btn-sm" onClick={() => buyBookMark()}>Buy Bookmark</button>
+                {bookmarks.length > 0 ? (
+                    bookmarks.map((item) => {
+                        return (
+                            <>
+                                <p>Price for bookmark: {curserial_num} {datamine}</p>
+                                <p>Contract Address: {item.item_bmcontract_address}</p>
+                                <p>Bookmark Price : {item.bookmarkprice}</p>
+                                <button type="button" className="btn btn-primary btn-sm" onClick={() => buyBookMark(item.item_bmcontract_address, item.bookmarkprice)}>Buy {item.tokenname} Bookmark</button>
+                            </>
+                        )
+                    })
+                ) : (
+                    <></>
+                )}
                 <hr/>
                 <p>Price for Hardbound: HB{datamine}</p>
-                <p>Contract Address: {hb_contract_address}</p>
-                <p>Hardbound Price : {hardbound_price}</p>
+                <p>Contract Address: {hbcontractaddress}</p>
+                <p>Hardbound Price : {hbprice}</p>
                 <button type="button" className="btn btn-primary btn-sm" onClick={() => buyBookHardbound()}>Buy Hardbound</button>
                 <hr/>
                 <h5>For owner <span id="ownerspan">{bmContractowner}</span></h5>
@@ -213,13 +249,13 @@ function BMdetailModal(props) {
                   to: <input type="text" id="toaddress" name="fname" size="42" value={tosendaddress} onChange={(e) => setTosendaddress(e.target.value)}/><br/>
                 <br/>
                 <hr/>
-                <h5>The Great Library's Bridge</h5>
+                {/* <h5>The Great Library's Bridge</h5>
                 <button type="button" className="btn btn-primary btn-sm" id="btn-apply-rates" onClick={() => applydexrates()}>Apply DEX rates</button>
                 X Rate: <input type="text" id="dexXMTSPRateId" size="5" defaultValue="" /> CC Rate: <input type="text" id="dexCCRateId" size="5" value={dexrate} onChange={(e) => setDexrate(e.target.value)} /><br/><br/>
                 <button type="button" className="btn btn-primary btn-sm" id="btn-find-rates" onClick={()=>finddexrates()}>Find new DEX rates</button>
                 <hr/>
                 <button type="button" className="btn btn-primary btn-sm" id="btn-set-stake-rate" onClick={()=>setCCstakerate()}>Set Stake Rate</button>
-                Stake Rate: <input type="text" id="stakerateid" size="5" value={stakerate} onChange={(e) => setStakerate(e.target.value)} />
+                Stake Rate: <input type="text" id="stakerateid" size="5" value={stakerate} onChange={(e) => setStakerate(e.target.value)} /> */}
                 <button type="button" className="btn btn-primary btn-sm" id="btn-get-stake-rate" onClick={()=>getstakerate()}>Get Stake Rate</button>
             </div>
         </Modal.Body>
